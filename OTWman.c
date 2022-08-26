90277b50785dea9da936e916ef0372513f57f566
dawg <100376322+bitsflipp3r@users.noreply.github.com>
2022-08-26 18:52:53 +0100

    First working solution

--- OTWman.c
+++ OTWman.c
@@ -1,7 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int len(char string[])
+{
+    int i = 0;
+    while(string[i] != '\0')
+    {
+        i++;
+    }
+    return i;
+}
+
 
 int main(int argc, char **argv[])
 {
-    printf("Hello World!\n");
+    char level[33];
+    char password[99];
+    FILE *fptr; // file pointer
+
+    fptr = fopen("/home/d4wg/Desktop/c/OTWlogs/OTWlogs.txt", "a+");
+
+    if(fptr == NULL){
+        printf("Something went wront. Error code 1.\n");
+        exit(1);
+    }
+
+
+    printf("Enter your current Level: ");
+    scanf("%s", level);
+
+    printf("Enter The Password for the Next Level: ");
+    scanf("%s", password);
+
+    fprintf(fptr, "%s", level);
+    for(int i = 0; i <= 16 - len(level) ; i++)
+    {
+        fprintf(fptr, " ");
+    }
+
+    fprintf(fptr, "-    %s\n", password);
+    fclose(fptr);
+
     return 0;
 }
